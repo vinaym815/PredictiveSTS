6d648e442bc6063bdf442fd651cbe0bd72ab4ff4
Vinay <vinaym815@gmail.comC>
2021-12-28 04:05:25 +0900

    Added results of 60pct deficit

--- inc/universalConsts.h
+++ inc/universalConsts.h
@@ -5,41 +5,40 @@
 #include "OpenSim.h"
 
 #define Standing                            // Commenting will switch to Sitting
-//#define Assisted                          // Commenting will switch to unAssisted
+#define Assisted                          // Commenting will switch to unAssisted
 
 // Sets up model and number of variables
 #ifdef Assisted
-  const std::string BASE_MODEL_NAME = "vin_60pctAssisted.osim";
-  const std::string OPTIM_MODEL_NAME = "vin_60pctAssisted.osim.mod";
-  const std::string REPLAY_MODEL_NAME = "vin_60pctAssisted_replay.osim.mod";
-
-  const int NUM_ACTUATORS = 10;                  // Number of unique excitation functions
-  #ifdef Standing
+    const std::string BASE_MODEL_NAME = "vin_60pctAssisted.osim";
+    const std::string OPTIM_MODEL_NAME = "vin_60pctAssisted.osim.mod";
+    const std::string REPLAY_MODEL_NAME = "vin_60pctAssisted_replay.osim.mod";
+    const int NUM_ACTUATORS = 10;                  // Number of unique excitation functions
+#ifdef Standing
     const size_t NUM_WEIGHTS = 10;
-  #else
+#else
     const size_t NUM_WEIGHTS = 8;
-  #endif
+#endif
 
 #else
-  //const std::string BASE_MODEL_NAME = "vin_00pct.osim";
-  //const std::string OPTIM_MODEL_NAME = "vin_00pct.osim.mod";
-  //const std::string REPLAY_MODEL_NAME = "vin_00pct_replay.osim.mod";
-  //const std::string BASE_MODEL_NAME = "vin_20pct.osim";
-  //const std::string OPTIM_MODEL_NAME = "vin_20pct.osim.mod";
-  //const std::string REPLAY_MODEL_NAME = "vin_20pct_replay.osim.mod";
-  const std::string BASE_MODEL_NAME = "vin_40pct.osim";
-  const std::string OPTIM_MODEL_NAME = "vin_40pct.osim.mod";
-  const std::string REPLAY_MODEL_NAME = "vin_40pct_replay.osim.mod";
-  //const std::string BASE_MODEL_NAME = "vin_60pct.osim";
-  //const std::string OPTIM_MODEL_NAME = "vin_60pct.osim.mod";
-  //const std::string REPLAY_MODEL_NAME = "vin_60pct_replay.osim.mod";
+    //const std::string BASE_MODEL_NAME = "vin_00pct.osim";
+    //const std::string OPTIM_MODEL_NAME = "vin_00pct.osim.mod";
+    //const std::string REPLAY_MODEL_NAME = "vin_00pct_replay.osim.mod";
+    //const std::string BASE_MODEL_NAME = "vin_20pct.osim";
+    //const std::string OPTIM_MODEL_NAME = "vin_20pct.osim.mod";
+    //const std::string REPLAY_MODEL_NAME = "vin_20pct_replay.osim.mod";
+    //const std::string BASE_MODEL_NAME = "vin_40pct.osim";
+    //const std::string OPTIM_MODEL_NAME = "vin_40pct.osim.mod";
+    //const std::string REPLAY_MODEL_NAME = "vin_40pct_replay.osim.mod";
+    const std::string BASE_MODEL_NAME = "vin_60pct.osim";
+    const std::string OPTIM_MODEL_NAME = "vin_60pct.osim.mod";
+    const std::string REPLAY_MODEL_NAME = "vin_60pct_replay.osim.mod";
 
-  const int NUM_ACTUATORS = 8; 
-  #ifdef Standing
-      const size_t NUM_WEIGHTS = 9;
-  #else
-      const size_t NUM_WEIGHTS = 7;
-  #endif
+    const int NUM_ACTUATORS = 8; 
+#ifdef Standing
+    const size_t NUM_WEIGHTS = 9;
+#else
+    const size_t NUM_WEIGHTS = 7;
+#endif
 #endif
 
 // Simulation Setup
@@ -56,13 +55,21 @@ const double DEFAULT_EXCITATION = 0.05;
 const SimTK::Vec3 COM_TARGET{0.0346674, 0.943719, -0.0546681};
 const int NUM_COMPS = 16;                   // Number of parameterization componenets each excitation trajectory is made up of
 const double TAU_CHAIR_FORCE = T_MAX/8.0;
-const std::string LOWER_BOUNDS_FILE = "lowerBound.txt";
-const std::string UPPER_BOUNDS_FILE = "upperBound.txt";
-const std::string STD_DEV_FILE = "stdDev.txt";
 const int N_RESTARTS = 5;
 const double F_TOLERANCE = 0.5;
 const int HIST_SIZE = 250;
 const int MAX_ITER = 4000;
+
+#ifdef Assisted
+    const std::string LOWER_BOUNDS_FILE = "lowerBoundAssisted.txt";
+    const std::string UPPER_BOUNDS_FILE = "upperBoundAssisted.txt";
+    const std::string STD_DEV_FILE = "stdDevAssisted.txt";
+#else
+    const std::string LOWER_BOUNDS_FILE = "lowerBound.txt";
+    const std::string UPPER_BOUNDS_FILE = "upperBound.txt";
+    const std::string STD_DEV_FILE = "stdDev.txt";
+#endif
+
 
 // Log file Names
 const std::string LOG_BEST_FILE = "logBest.txt";
